b45a09b8fe75290657dedf9a4c7b068d8f736086
Kristina Chodorow <kristina@Macintosh.local>
2009-07-01 15:04:35 -0400

    csv format

--- insert-bench.php
+++ insert-bench.php
@@ -3,7 +3,7 @@
 $config = array(
 	'method' => 'bulk_insert',
 	'insertCounts' => array(
-		0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+                                /*		0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
 		10,
 		50,
 		100,
@@ -19,9 +19,13 @@ $config = array(
 		250000,
 		500000,
 		750000,
-		1000000,
+		1000000,*/
 	)
 );
+
+for($i=7000000; $i<=10000000; $i+=1000000) {
+    $config['insertCounts'][] = $i;
+}
 
 $m = new Mongo();
 $c = $m->selectCollection('test', 'benchmark');
@@ -30,7 +34,7 @@ foreach ($config['insertCounts'] as $docCount) {
   // Re-create the database for each attempt
   $c->drop();
 
-  echo sprintf("-> %s %d docs:\n", $config['method'], $docCount);
+  //  echo sprintf("-> %s %d docs:\n", $config['method'], $docCount);
 
   switch ($config['method']) {
   case 'bulk_insert':
@@ -40,7 +44,7 @@ foreach ($config['insertCounts'] as $docCount) {
       $insertAtOnce = ($docCount - $docsWritten > 1000)
         ? 1000
         : $docCount - $docsWritten;
-      
+
       $docs = array();
       for ($i = 0; $i < $insertAtOnce; $i++) {
         $docs[] = array(
@@ -50,8 +54,8 @@ foreach ($config['insertCounts'] as $docCount) {
       }
       $c->batchInsert($docs);
       $docsWritten = $docsWritten + $insertAtOnce;
-      echo '.';
     }
+    $c->findOne();
     $insertEnd = microtime(true);
     break;
   case 'single_insert':
@@ -60,17 +64,23 @@ foreach ($config['insertCounts'] as $docCount) {
       $c->insert(array('_id' => new MongoId(), 'foo' => 'bar'));
     }
     $insertEnd = microtime(true);
-    echo '.';
     break;
   }
-  
+
+  //  $c->findOne();
+
   $beforeCompact = array('stats' => array('doc_count' => $docCount, 'disk_size' => 0), 'fileSize' => 0);
   $afterCompact = array('stats' => array('doc_count' => $docCount, 'disk_size' => 0), 'fileSize' => 0);
   $compactStart = 0;
   $compactEnd = 0;
 
-  echo "\n\n";
-  echo sprintf(
+  echo $docCount.",";
+  echo round($insertEnd - $insertStart, 4).",";
+  echo ($beforeCompact['stats']['doc_count'])
+      ? ((($insertEnd - $insertStart) * 1000) / $beforeCompact['stats']['doc_count'])
+      : 'n/a';
+  echo "\n";
+  /*  echo sprintf(
                "doc count (before compact): %s\n".
                "insert time: %s sec\n".
                "insert time / doc: %s ms\n",
@@ -83,8 +93,8 @@ foreach ($config['insertCounts'] as $docCount) {
                round($compactEnd - $compactStart, 4),
                ($beforeCompact['stats']['doc_count'])
                ? ((($compactEnd - $compactStart) * 1000) / $beforeCompact['stats']['doc_count'])
-               : 'n/a' 
-               );
+               : 'n/a'
+               );*/
 }
 
 
